2d7ebefa3fa51589c384c7cd1191fdb7a3a6537c
Andronnix <w4UvOQY4LvLa>
2017-04-09 14:19:11 +0300

    Book addition implemented

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,10 +2,10 @@
  * Created by Andro on 09 апреля.
  */
 
-myApp = angular.module('myApp', []);
+myApp = angular.module('myApp', ['ui.bootstrap']);
 
 (function(app) {
-    app.controller('BooksListController', function ($scope, $http) {
+    app.controller('BooksListController', function ($scope, $http, $log, $uibModal) {
         $scope.books = [];
 
         $http({
@@ -16,8 +16,53 @@ myApp = angular.module('myApp', []);
                 $scope.books = request.data;
             },
             (err) => {
-                console.error("Something bad happened", err)
+                $log.error("Something bad happened", err)
             }
         );
+
+        $scope.addBook = function (size) {
+            const modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$ctrl',
+                size: size,
+                resolve: { }
+            });
+
+            modalInstance.result.then(saveBook, () => {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        function saveBook(book) {
+            $http({
+                method: 'POST',
+                url: '/books',
+                data: book,
+            }).then(
+                () => {
+                    $scope.books.push(book);
+                },
+                (err) => {
+                    $log.error("Can't add book, err:", err.data)
+                }
+            );
+        }
+    });
+
+    app.controller('ModalInstanceCtrl', function ($uibModalInstance) {
+        const $ctrl = this;
+        $ctrl.book = { };
+
+        $ctrl.ok = function () {
+            $uibModalInstance.close($ctrl.book);
+        };
+
+        $ctrl.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
     });
 })(myApp);
